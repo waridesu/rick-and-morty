11d0f3ef378a14f6ceeec6fd6ed3a600ea8d3e7f
AntoninaHoncharenko <104636066+AntoninaHoncharenko@users.noreply.github.com>
2023-05-25 17:05:28 +0200

    add mob menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  isOpenMenu = false;
 
+  toggleMenu() {
+    this.isOpenMenu = !this.isOpenMenu;
+  }
 }
